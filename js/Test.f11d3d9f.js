00e6ed55684f74339a90609ae1efbc8cb9166d78
xieyufei <xieyufei@boe.com.cn>
2023-04-13 13:18:29 +0800

    add

---
+++ js/Test.f11d3d9f.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkcreation_gallery"]=self["webpackChunkcreation_gallery"]||[]).push([[12],{2297:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var a=n(3396);const r={name:"Test1"};var s=Object.assign(r,{setup(e){return(e,t)=>((0,a.wg)(),(0,a.iD)("div",null,"test"))}}),c=n(89);const u=(0,c.Z)(s,[["__scopeId","data-v-5e682bb5"]]);var l=u}}]);
